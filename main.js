15238b03fc58850e5d6e843ebd05cfe7629c2eae
Dhruvrao1205 <102854866+Dhruvrao1205@users.noreply.github.com>
2022-04-04 12:02:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function Change() {
+    document.getElementById('id').style.backgroundColor="black";
+    document.getElementById('id').style.color="aquamarine";
+    document.getElementById('btn').style.color="bisque";
+    document.getElementById('btn').style.backgroundColor="brown";
+    document.getElementById('btn').style.color="bisque";
+    document.getElementById('btn').style.borderColor="bisque";
+}
